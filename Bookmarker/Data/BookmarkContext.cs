2f4154c5ace07358014469ca6511a3e5b42d5ec9
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-04-26 08:51:59 -0400

    fix: Add snowflake converter to database

--- Bookmarker/Data/BookmarkContext.cs
+++ Bookmarker/Data/BookmarkContext.cs
@@ -1,8 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using PalladiumUtils;
+using Remora.Rest.Core;
 
 namespace Bookmarker.Data;
 
 public class BookmarkContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<BookmarkEntity> Bookmarks { get; set; }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+
+        configurationBuilder.Properties<Snowflake>().HaveConversion<SnowflakeConverter>();
+        configurationBuilder.Properties<Snowflake?>().HaveConversion<SnowflakeNullableConverter>();
+    }
 }
