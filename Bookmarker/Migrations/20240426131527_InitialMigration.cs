f26371345b02ed2e4b02bcecda7ade89df574a59
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-04-26 09:16:55 -0400

    chore: Add migration

---
+++ Bookmarker/Migrations/20240426131527_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookmarker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookmarks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Tags = table.Column<string>(type: "TEXT", nullable: false),
+                    Attachments = table.Column<string>(type: "TEXT", nullable: false),
+                    PartialContent = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    AuthorID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ChannelID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    MessageID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    GuildID = table.Column<ulong>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookmarks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookmarks");
+        }
+    }
+}
