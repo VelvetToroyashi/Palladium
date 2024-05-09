e89bdf3c358f96fab51ae0867f2862c44776863d
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-05-09 02:51:06 -0400

    refactor: Remove converter hints from database converters

--- PalladiumUtils/DatabaseConverters.cs
+++ PalladiumUtils/DatabaseConverters.cs
@@ -8,18 +8,16 @@ public static class DatabaseConverterExtensions
 {
 }
 
-public class SnowflakeConverter(ConverterMappingHints? mappingHints = null) : 
+public class SnowflakeConverter() : 
     ValueConverter<Snowflake, ulong>
     (
         id => id.Value,
-        value => new Snowflake(value, Constants.DiscordEpoch),
-        mappingHints
+        value => new Snowflake(value, Constants.DiscordEpoch)
     );
     
-public class SnowflakeNullableConverter(ConverterMappingHints? mappingHints = null) :
+public class SnowflakeNullableConverter() :
     ValueConverter<Snowflake?, ulong?>
     (
         id => id.HasValue ? id.Value.Value : null,
-        value => value.HasValue ? new Snowflake(value.Value, Constants.DiscordEpoch) : null,
-        mappingHints
+        value => value.HasValue ? new Snowflake(value.Value, Constants.DiscordEpoch) : null
     );
